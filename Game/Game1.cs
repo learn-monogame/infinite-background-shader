ce005c26f82ea3adc66f650ceef1e2a7a22d1998
Jean-David Moisan <vynecompiler@gmail.com>
2021-03-25 18:49:25 -0400

    Fix use texture parameter

--- Game/Game1.cs
+++ Game/Game1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Apos.Input;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -134,7 +134,7 @@ namespace GameProject {
         }
         private Matrix GetUVTransform(Texture2D t, Vector2 offset, float scale, Viewport v) {
             return
-                Matrix.CreateScale(_background.Width, _background.Height, 1f) *
+                Matrix.CreateScale(t.Width, t.Height, 1f) *
                 Matrix.CreateScale(scale, scale, 1f) *
                 Matrix.CreateTranslation(offset.X, offset.Y, 0f) *
                 GetView() *
